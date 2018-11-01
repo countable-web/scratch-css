b191042e04614c271c3028eae62f14528610ba0d
Joao Paulo Furtado Silva <joaopaulofurtado@live.com>
2018-10-31 20:04:14 -0700

    new styling to code box

--- angular/app.js
+++ angular/app.js
@@ -1,10 +1,11 @@
-let app = angular.module("app", ['ngRoute']);
+let app = angular.module("app", ['ngRoute','angularCSS']);
 
 
 app.config(function($routeProvider) {
     $routeProvider
         .when("/", {
-            templateUrl : "views/forms.html"
+            templateUrl : "views/forms.html",
+
         })
         .when("/buttons", {
             templateUrl : "views/buttons.html"
